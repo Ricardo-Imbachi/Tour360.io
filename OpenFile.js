323f3a3c7d00c8b9dbc3f086ee64bc394bad8d0c
Ricardo Imbachi <ricardoimbachi29@gmail.com>
2024-06-18 21:11:27 -0500

    Test Change Bg Image

--- OpenFile.js
+++ OpenFile.js
@@ -1,25 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const inputElement = document.getElementById('fileElem');
+  inputElement.addEventListener('change', ChangeBG);
+});
+
 function handleFiles(files) {
-    var d = document.getElementById("fileList");
-    if (!files.length) {
-      d.innerHTML = "<p>¡No se han seleccionado archivos!</p>";
-    } else {
-      var list = document.createElement("ul");
-      d.appendChild(list);
-      for (var i=0; i < files.length; i++) {
-        var li = document.createElement("li");
-        list.appendChild(li);
-  
-        var img = document.createElement("img");
-        img.src = window.createBlobURL(files[i]);;
-        img.height = 60;
-        img.onload = function() {
-          window.revokeBlobURL(this.src);
-        }
-        li.appendChild(img);
-  
-        var info = document.createElement("span");
-        info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
-        li.appendChild(info);
-      }
+  console.log("yesss");
+  var d = document.getElementById("fileList");
+  if (!files.length) {
+    d.innerHTML = "<p>¡No se han seleccionado archivos!</p>";
+  } else {
+    var list = document.createElement("ul");
+    d.appendChild(list);
+    for (var i=0; i < files.length; i++) {
+      var li = document.createElement("li");
+      list.appendChild(li);
+
+      var img = document.createElement("img");
+  img.classList.add("obj");
+  img.file = file;
+  preview.appendChild(img);
+
+  var reader = new FileReader();
+  reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+  reader.readAsDataURL(file);
     }
   }
+} 
+function ChangeBG(files)
+{
+  // var d = document.getElementById("BG");
+  // d.src="BG_02.jpg";
+  const imagen = document.getElementById('BG');
+  const archivo = files.target.files[0];
+            if (archivo) {
+                // Crear un objeto URL para el archivo seleccionado
+                const urlImagen = URL.createObjectURL(archivo);
+                // Cambiar el src de la imagen
+                imagen.src = urlImagen;
+            }
+}
+   
