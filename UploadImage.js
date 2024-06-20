9a841c7e7aab4eb993c73df062484bba7397ad0a
Ricardo Imbachi <ricardoimbachi29@gmail.com>
2024-06-19 19:08:10 -0500

    Update UploadImage.js

--- UploadImage.js
+++ UploadImage.js
@@ -10,50 +10,53 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
             }
             return response.text();
         })
-        .then(data => {
-            key = data.replace("\n", "")
-            console.log('Contenido del archivo:', textData);
-            
+        // .then(keyvalue => {
+        //     key = keyvalue.replace(/(\r\n|\n|\r)/gm, "");
+        // })
+        .then(keyvalue => {
+            key = keyvalue.replace(/(\r\n|\n|\r)/gm, "");
+            const fileInput = document.getElementById('fileInput');
+            const file = fileInput.files[0];
+        
+            if (!file) {
+                alert('Por favor, selecciona una imagen.');
+                return;
+            }
+        
+            const reader = new FileReader();
+            reader.onload = function() {
+                const base64Image = reader.result.split(',')[1];
+        
+                const data = {
+                    message: "Subiendo una imagen desde una página web",
+                    content: base64Image
+                };
+                console.log(key);
+        
+                fetch('https://api.github.com/repos/ricardo-imbachi/Tour360.io/contents/ImageTest/' + file.name, {
+                    method: 'PUT',
+                    headers: {
+                        Authorization: "Bearer "+key,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(response => {
+                    if (response.ok) {
+                        alert('Imagen subida con éxito!');
+                    } else {
+                        alert('Error al subir la imagen: ' + response.statusText);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Ocurrió un error al subir la imagen.');
+                });
+            };
+            reader.readAsDataURL(file);
         })
         .catch(error => {
             console.error('Hubo un problema con la solicitud fetch:', error);
         });
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
 
-    if (!file) {
-        alert('Por favor, selecciona una imagen.');
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = function() {
-        const base64Image = reader.result.split(',')[1];
-
-        const data = {
-            message: "Subiendo una imagen desde una página web",
-            content: base64Image
-        };
-
-        fetch('https://api.github.com/repos/ricardo-imbachi/Tour360.io/contents/ImageTest/' + file.name, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `Bearer ${key}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => {
-            if (response.ok) {
-                alert('Imagen subida con éxito!');
-            } else {
-                alert('Error al subir la imagen: ' + response.statusText);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Ocurrió un error al subir la imagen.');
-        });
-    };
-    reader.readAsDataURL(file);
 });
